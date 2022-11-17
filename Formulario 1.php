84dac4edca6036ebcf6c560b5e5e009a1a3d4c35
Brayan <brayanmartinez07149@gmail.com>
2022-11-17 11:12:09 -0400

    Se agrego

--- Formulario 1.php
+++ Formulario 1.php
@@ -170,6 +170,42 @@
 
         <input type="submit" class="btn btn-primary" name="enviar" value="aceptar"/>
     </form>
-    
+    <?php 
+
+require("conexion F1.php");
+insertar($mysqli);
+function insertar ($mysqli){
+    $graduacion=$_POST['graduacion'] ?? null;
+    $Institucion=$_POST['Institucion'] ?? null; 
+    $Curso=$_POST['Curso'] ?? null;
+    $Matricula=$_POST['Matricula'] ?? null; 
+    $Cedula=$_POST['Cedula'] ?? null;
+    $CarerraTecnica=$_POST['CarerraTecnica'] ?? null; 
+    $TecnicoBasico=$_POST['TecnicoBasico'] ?? null;
+    $Nombre=$_POST['Nombre'] ?? null;
+    $apellidos=$_POST['apellidos'] ?? null;
+    $fechaNacimiento=$_POST['fechaNacimiento'] ?? null;
+    $sexoN=$_POST['sexoN'] ?? null;
+    $direccion = $_POST['direccion']?? null;
+    $sector=$_POST['sector'] ?? null;
+    $Seccion=$_POST['Seccion'] ?? null;
+    $Municipio=$_POST['Municipio'] ?? null;
+    $Provincia=$_POST['Provincia'] ?? null;
+    $Nacionalidad=$_POST['Nacionalidad'] ?? null;
+    $TelefonoResidencial=$_POST['TelefonoResidencial'] ?? null;
+    $TelefonoMovil=$_POST['TelefonoMovil'] ?? null;
+    $Licenciaconducir=$_POST['Licenciaconducir'] ?? null;
+    $Vehiculo=$_POST['Vehiculo'] ?? null;
+    $email=$_POST['email'] ?? null;
+    $clave=$_POST['clave'] ?? null;
+
+
+    $query= "insert into alumnos values(default,'$graduacion','$Institucion','$Curso','$Matricula','$Cedula','$CarerraTecnica','$TecnicoBasico','$Nombre','$apellidos','$fechaNacimiento','$sexoN','$direccion','$sector','$Seccion','$Municipio','$Provincia','$Nacionalidad','$TelefonoResidencial','$TelefonoMovil','$Licenciaconducir','$Vehiculo','$email','$clave')";
+
+    $result=mysqli_query($mysqli,$query);
+}
+mysqli_close($mysqli);
+
+?>
     </div>
 </body>
---
+++ Sql/Estudiante.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <form action="" method="post">
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>graduacion</th>
+            <th>institucion educativa</th>
+            <th>Curso</th>
+            <th>Matricula</th>
+            <th>Cedula</th>
+            <th>carrera tecnica</th>
+            <th>tecnico basico</th>
+            <th>Nombres</th>
+            <th>Apellidos</th>
+            <th>fecha de nacimiento</th>
+            <th>sexo</th>
+            <th>sector</th>
+            <th>seccion</th>
+            <th>municipio</th>
+            <th>provincia</th>
+            <th>nacionalidad</th>
+            <th>tel_res</th>
+            <th>num_cel</th>
+            <th>licencia</th>
+            <th>vehiculo</th>
+            <th>email</th>
+            <th>contraseña</th>
+            <th>direccion</th>
+        </tr>
+        <?php?>
+include_once 'Conexion F1.php'
+
+
+    </table>
+
+
+
+
+
+   </form> 
+</body>
+</html>
